bff1bcaff6cbc98f62c82bc05b9e67f8478ee538
Luanna <lua.varjao@gmail.com>
2023-07-28 21:24:21 -0300

    Resolução Ex 02

---
+++ 02/index.js
@@ -0,0 +1,27 @@
+const carros =[
+    {
+    marca: "Chevrolet",
+    modelo: "Cruze",
+    ano: 2017,
+    cor: "prata",
+    quantidade_portas: 4,
+    automatico: true
+},{
+    marca: "Renault",
+    modelo: "Kwid",
+    ano: 2019,
+    cor: "branco",
+    quantidade_portas: 4,
+    automatico: false
+} , {
+    marca: "Chevrolet",
+    modelo: "Onix",
+    ano: 2014,
+    cor: "preto",
+    quantidade_portas: 4,
+    automatico: true
+}
+
+]
+
+console.log(carros[2]);
