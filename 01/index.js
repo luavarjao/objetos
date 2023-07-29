fba42ee66f760aac52ae47a5f71aa03ca3c6cca0
Luanna <lua.varjao@gmail.com>
2023-07-28 21:17:11 -0300

    Resolução Ex 01

---
+++ 01/index.js
@@ -0,0 +1,21 @@
+const endereco ={
+    rua: "Pernambuco",
+    numero: 405,
+    complemento: "apto",
+    bairro: "Pituba",
+    cep: 41600830,
+    cidade: "salvador",
+    estado: "BA",
+    pais: "Brasil"
+}
+
+const usuario ={
+    nome: "Luanna",
+    email: "lua.varjao",
+    telefone: 7199999999,
+    data_nascimento: "28/06/1991",
+    endereco
+}
+
+
+console.log(endereco);
