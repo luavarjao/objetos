5414af065159dbc157df43a8585b2a817edc0965
Luanna <lua.varjao@gmail.com>
2023-08-02 21:30:07 -0300

    Resolução Ex 05

---
+++ 05/index.js
@@ -0,0 +1,28 @@
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: [],
+}
+
+let aula = [
+{
+    identificador: 1,
+    nome_da_aula: "Introdução a programação"
+},
+{
+    identificador: 2,
+    nome_da_aula: "Variáveis"
+},
+{
+    identificador: 3,
+    nome_da_aula: "Condicionais"
+},
+{
+    identificador: 4,
+    nome_da_aula: "Arrays"
+}
+]
+
+curso.aulas = aula;
+
+console.log(curso);
