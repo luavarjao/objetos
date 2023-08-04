ca41844c05f3e1cd0aa407ea26bb5610e70818be
Luanna <lua.varjao@gmail.com>
2023-08-04 16:16:20 -0300

    Resolução Ex 09

---
+++ 09/index.js
@@ -0,0 +1,29 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+]
+
+let x =0;
+
+for (let i of participantes){
+    if (i.nome === "Carlos"){
+        console.log(`Galera... O Carlos está na posição ${x + 1}, corre lá!`)
+    }
+    x++;
+}
