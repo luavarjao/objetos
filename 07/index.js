343cc4308192a661b4ad01d21ec813eeb8844f8c
Luanna <lua.varjao@gmail.com>
2023-08-04 15:34:46 -0300

    Resolução Ex 07

---
+++ 07/index.js
@@ -0,0 +1,42 @@
+const livro = {
+    nome: 'O poder do hábito',
+    capitulos: [
+        {
+            nome: "O loop do hábito",
+        },
+        {
+            nome: "O cérebro ansioso",
+        },
+        {
+            nome: "A regra de ouro da mudança de hábito",
+        },
+        {
+            nome: "Hábitos angulares, ou a balada de Paul O`neill",
+        },
+        {
+            nome: "Starbucks e o hábito do sucesso",
+        },
+        {
+            nome: "O poder de uma crise",
+        },
+        {
+            nome: "Como a target sabe o que você quer antes que você saiba",
+        },
+        {
+            nome: "A Saddleback Church e o boicote aos ônibus de Montgomery",
+        },
+        {
+            nome: "A neorologia do livre-arbítrio",
+        },
+    ]
+}
+let x = 1;
+let info = [];
+
+for (let i of livro.capitulos){
+    info.push({numero: x, ...livro.capitulos[x-1]});
+    x++;
+}
+
+console.log(info)
+
